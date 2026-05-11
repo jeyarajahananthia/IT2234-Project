d6837ee082055d491fddf0db7517c221d1d9aa8d
Ananthia <anajeya26@gmail.com>
2026-05-11 11:31:21 +0530

    Added model folder

---
+++ model/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema({
+    bookName: {
+        type : String,
+        required : true,
+    },
+
+    Author: {
+        type : String,
+        required : true,
+    },
+
+    price: {
+        type : Number,
+        required : true,
+    },
+
+    condition: {
+        type : String,
+        required : true,
+    },
+
+    ownerName : {
+        type : String,
+        required : true,
+    },
+
+    contactEmail: {
+        type : String,
+        required : true,
+        unique : true,
+    }
+});
+
+export default mongoose.model("Books",BookSchema);
